ac0ae6b73bf779b7b172deef13a61f4a1fb2fb62
vusumzi123 <D.O.GeeVz@gmail.com>
2016-12-13 14:25:47 -0600

    changed to object oriented paradigm version 0.2.1

--- js/main.js
+++ js/main.js
@@ -1,84 +1,39 @@
+var main = function(vars) {
+  var scale = 40;
+  var canvas = new CanvasHead(vars);
 
-var cent=canvas.width/2;
-var sc=40;  
+  canvas.background('#eee');
 
+  function drawPlane(){
 
-background("#EEEEEE");
+  canvas.strokeWeight(0.2);
+  canvas.line(canvas.centerW,0,canvas.centerW,canvas.height);
+  canvas.line(0,canvas.centerH,canvas.width,canvas.centerW);
 
+      for(var i=0;i<=canvas.centerW;i++){
 
-function drawPlane(){
-
-
-line(canvas.height/2,0,canvas.height/2,canvas.width);
-line(0,canvas.width/2,canvas.height,canvas.width/2);
-    
-
-    for(var i=0;i<=canvas.width/2;i++){
-        
-        if(i%sc===0){
-            fill(255, 0, 0);
-            strokeWeight(2);
-            point(cent+i,cent);
-            point(cent-i,cent);
-            point(cent,cent+i);
-            point(cent,cent-i);
-            fill(255, 0, 0);
-            if(i>0){
-            	strokeWeight(1);
-            	fill("black");
-                text(-i/sc, cent-11, cent+i,12);
-                text(i/sc, cent+11, cent-i,12);
-                text(i/sc, cent+i, cent+14,12);
-                text(-i/sc, cent-i, cent-8,12);
-            }
-        }
-        
-    }
-
-};
+          if(i%scale===0){
+              canvas.fill(255, 0, 0);
+              canvas.strokeWeight(7);
+              canvas.point(canvas.centerW+i,canvas.centerH);
+              canvas.point(canvas.centerW-i,canvas.centerH);
+              canvas.point(canvas.centerW,canvas.centerH+i);
+              canvas.point(canvas.centerW,canvas.centerH-i);
 
 
-function fun(x){
-    
-    
-    this.y=x;
-    
-    return this.y*sc*(-1);
-};
+              if(i>0){
+              	canvas.strokeWeight(1);
+              	canvas.fill("black");
+                canvas.text(-i/scale, canvas.centerW-11, canvas.centerH+i,12);
+                canvas.text(i/scale, canvas.centerW+11, canvas.centerH-i,12);
+                canvas.text(i/scale, canvas.centerW+i, canvas.centerH+14,12);
+                canvas.text(-i/scale, canvas.centerW-i, canvas.centerH-8,12);
+              }
+          }
 
+      }
 
-var x=0;
+  };
 
-
-function plott(){
-        
-        strokeWeight(2);
-        stroke(0, 16, 237);
-        def=0.2;
-        
-        
-        //pointP(x1,x2);
-        //pointN(x1,x2);
-
-        line(center+x*sc,center+fun(x),center+(x+def)*sc,center+fun(x+def));
-        line(center+(-1)*x*sc,center+fun((-1)*x),center+((-1)*x+def)*sc,center+fun((-1)*x+def));
-
-        x+=def;
-
-
-        //if(ang<Math.PI){
-
-        	requestAnimationFrame(plott);
-        	//console.log("finished");
-        //}
-
-
-};
-
-
-
-drawPlane();
-
-
-plott();
-
+  drawPlane();
+}
